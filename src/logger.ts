909129a5814b8d14fd364d7076d3fc516e37cd1e
mrpinkcat <gatien.anizan@gmail.com>
2019-12-05 02:46:24 +0100

    Added exact time to log

--- src/logger.ts
+++ src/logger.ts
@@ -2,8 +2,11 @@ import { createLogger, transports, format } from 'winston';
 
 export default createLogger({
   level: 'info',
-  format: format.json(),
-  defaultMeta: { timestamp: new Date().getTime(), service: 'reddit-assistant' },
+  format: format.combine(
+    format.timestamp(),
+    format.json(),
+  ),
+  defaultMeta: { service: 'reddit-assistant' },
   transports: [
     new transports.Console(),
     new transports.File({ filename: `./logs/error/logs error ${new Date()}.log`, level: 'error' }),
